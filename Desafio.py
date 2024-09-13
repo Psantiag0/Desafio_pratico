e1bca3fbea4334e0fc380e15198d5f52feddd72d
Paulo Santiago <paulokbr@gmail.com>
2024-09-13 17:36:01 -0300

    Dicionário criado

---
+++ Desafio.py
@@ -0,0 +1,11 @@
+# Implementar um programa que faça a análise de vendas de uma lista de produtos. Cada produto deverá ser um dicionário contendo o nome do produto,
+# o preço unitário do produto e a quantidade vendida
+
+
+produtos = [
+    {"nome": "Notebook", "preco_unitario": 3500.00, "quantidade_vendida": 10},
+    {"nome": "Teclado", "preco_unitario": 120.00, "quantidade_vendida": 50},
+    {"nome": "Mouse", "preco_unitario": 80.00, "quantidade_vendida": 40},
+    {"nome": "Monitor", "preco_unitario": 900.00, "quantidade_vendida": 15},
+    {"nome": "Impressora", "preco_unitario": 450.00, "quantidade_vendida": 8}
+]
